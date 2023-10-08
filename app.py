2f9d613de01b0e8cf8879334729b61d00c0bb7be
rajramch <r.ramchandani.rj@gmail.com>
2023-10-08 15:49:47 -0700

    added crop logic dependent on aspect ratio

--- app.py
+++ app.py
@@ -21,6 +21,21 @@ def change_image(img):
     inky = Inky_Impressions_7()
     saturation = 0.5
     image = Image.open(img)
-    resizedimage = image.resize(inky.resolution)
+
+    img_asp_rat = image.width /image.height
+    if(img_asp_rat < 800 / 480):
+        multiplier = image.width / 800
+        new_height = 480 * multiplier
+        diff = image.height - new_height
+        diff = int(diff/2)
+        crop_image = image.crop([0, diff , image.width , diff + new_height])
+    else:
+        multiplier = image.height / 480
+        new_width = 800 * multiplier
+        diff = image.width - new_width
+        diff = int(diff/2)
+        crop_image = image.crop([diff, 0, diff + new_width, image.height])
+
+    resizedimage = crop_image.resize(inky.resolution)
     inky.set_image(resizedimage, saturation=saturation)
     inky.show()
